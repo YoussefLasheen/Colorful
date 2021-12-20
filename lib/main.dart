a21d770dc1f8610f922dff2b81e5a43d032bbe83
Paul Köhler <koehler_paul@outlook.de>
2021-12-20 13:52:17 +0100

    changed theme and Name⏫

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,18 @@ import 'package:colorful/screens/main_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      title: 'Colorful',
+      theme: ThemeData(),
       home: const MainScreen(),
     );
   }
